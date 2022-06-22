da4a230ef4e3d31d022f50c8da7962dcdb4ec421
ChangappaMA <mukkatira.changappa@gmail.com>
2022-06-22 23:27:26 +0530

    Intro to CURD on mongo with python

--- MongoDB/pymongodemo.py
+++ MongoDB/pymongodemo.py
@@ -7,8 +7,17 @@ cluster = MongoClient("mongodb+srv://admin:admin123@cluster0.ogibm.mongodb.net/?
 db = cluster["sldb"]
 collection = db["employee"]
 
-post1 = {"name": "Changappa", "email":"chang@abcd.com"}
-post2 = {"name": "Changappa2", "email":"chang2@abcd.com"}
+post1 = {"_id": 101,"name": "Changappa", "email":"chang@abcd.com"}
+post2 = {"_id": 102,"name": "Changappa2", "email":"chang2@abcd.com"}
 
 
-collection.insert_many([post1, post2])
+# collection.insert_many([post1, post2])
+
+# results = collection.find_one({"name": "Changappa"})
+# print(results["email"])
+
+# for result in results:
+#     print(result["email"])
+
+# results= collection.delete_many({"name" : "Changappa"})
+collection.update_one({"_id": 102}, {"$set" : {"name": "Changappa123"}})
